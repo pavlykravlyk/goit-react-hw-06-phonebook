d21e4323d617d3bb6ed51aafedebc7296508c2d5
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-08 23:17:13 +0100

    Delete contacts-types.js

--- src/redux/contacts/contacts-types.js
+++
@@ -1,8 +0,0 @@
-const contactsTypes = {
-  ADD: 'contacts/add',
-  DELETE: 'contacts/delete',
-  FIND_CONTACTS_BY_NAME: 'contacts/findByName',
-  FILTER_CONTACTS_LIST: 'contacts/filterList',
-};
-
-export default contactsTypes;
