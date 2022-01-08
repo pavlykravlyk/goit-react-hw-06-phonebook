2cbaa189d17706f33f15ee02bc35f2e1f54b6a05
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-08 01:02:02 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,19 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import contactsReducer from './contacts/contacts-reducer';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-});
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+// });
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+  devTools: process.env.NODE_ENV === 'development',
+});
 
 export default store;
