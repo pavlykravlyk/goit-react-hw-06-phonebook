fc74c75b0b2d63118d8b8f94058df3c4adbdfdd0
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-08 23:17:21 +0100

    Update contacts-actions.js

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,23 +1,6 @@
-// import { nanoid } from 'nanoid';
 import { createAction, nanoid } from '@reduxjs/toolkit';
-// import contactsTypes from './contacts-types';
 
-// const addContact = (name, number) => ({
-//   type: contactsTypes.ADD,
-//   payload: { id: nanoid(5), name, number },
-// });
-
-// const deleteContact = contactId => ({
-//   type: contactsTypes.DELETE,
-//   payload: contactId,
-// });
-
-// const findContactsByName = value => ({
-//   type: contactsTypes.FIND_CONTACTS_BY_NAME,
-//   payload: value,
-// });
-
-const addContact = createAction('contacts/add', (name, number) => ({
+const addContact = createAction('contacts/addContact', (name, number) => ({
   payload: {
     id: nanoid(),
     name,
@@ -25,9 +8,8 @@ const addContact = createAction('contacts/add', (name, number) => ({
   },
 }));
 
-const deleteContact = createAction('contacts/delete');
-
-const findContactsByName = createAction('contacts/findByName');
+const deleteContact = createAction('contacts/deleteContact');
+const findContactsByName = createAction('contacts/findContactByName');
 
 const contactActions = { addContact, deleteContact, findContactsByName };
 
