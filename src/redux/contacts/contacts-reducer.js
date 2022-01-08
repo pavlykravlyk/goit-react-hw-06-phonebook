42225db99c52ee1503053bde589aab36bb9495f9
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-08 23:17:16 +0100

    Update contacts-reducer.js

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,42 +1,21 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
-import types from './contacts-types';
-import initContacts from '../../contacts.json';
-
-// const items = (state = initialContacts, { type, payload }) => {
-//   switch (type) {
-//     case types.ADD:
-//       return state.some(({ name }) => payload.name === name)
-//         ? alert(`${payload.name} is already in contacts`)
-//         : [...state, payload];
-
-//     case types.DELETE:
-//       return state.filter(({ id }) => id !== payload);
-
-//     default:
-//       return state;
-//   }
-// };
+import actions from './contacts-actions';
+import initContacts from '../../initContacts.json';
 
 const items = createReducer(initContacts, {
-  [types.ADD]: (state, { payload }) =>
+  [actions.addContact]: (state, { payload }) =>
     state.some(contact => payload.name === contact.name)
       ? alert(`${payload.name} is already in contacts`)
       : [...state, payload],
 
-  [types.DELETE]: (state, { payload }) =>
+  [actions.deleteContact]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
 });
 
-const filter = (state = '', { type, payload }) => {
-  switch (type) {
-    case types.FIND_CONTACTS_BY_NAME:
-      return payload;
-
-    default:
-      return state;
-  }
-};
+const filter = createReducer('', {
+  [actions.findContactsByName]: (_, { payload }) => payload,
+});
 
 export default combineReducers({
   items,
