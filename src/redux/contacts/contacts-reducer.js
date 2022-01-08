4e9babda211805586792c11eb04a6443db78ed60
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-08 01:01:57 +0100

    Update contacts-reducer.js

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,21 +1,32 @@
 import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
 import types from './contacts-types';
-import initialContacts from '../../contacts.json';
+import initContacts from '../../contacts.json';
 
-const items = (state = initialContacts, { type, payload }) => {
-  switch (type) {
-    case types.ADD:
-      return state.some(({ name }) => payload.name === name)
-        ? alert(`${payload.name} is already in contacts`)
-        : [...state, payload];
+// const items = (state = initialContacts, { type, payload }) => {
+//   switch (type) {
+//     case types.ADD:
+//       return state.some(({ name }) => payload.name === name)
+//         ? alert(`${payload.name} is already in contacts`)
+//         : [...state, payload];
 
-    case types.DELETE:
-      return state.filter(({ id }) => id !== payload);
+//     case types.DELETE:
+//       return state.filter(({ id }) => id !== payload);
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
+
+const items = createReducer(initContacts, {
+  [types.ADD]: (state, { payload }) =>
+    state.some(contact => payload.name === contact.name)
+      ? alert(`${payload.name} is already in contacts`)
+      : [...state, payload],
+
+  [types.DELETE]: (state, { payload }) =>
+    state.filter(({ id }) => id !== payload),
+});
 
 const filter = (state = '', { type, payload }) => {
   switch (type) {
