a4b18a344e19360d74b81be238690d2f71244d53
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-08 23:17:37 +0100

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import contactActions from 'redux/contacts/contacts-actions';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './Filter.module.css';
 
 const Filter = ({ contactName, onFindContact }) => {
@@ -27,9 +27,9 @@ const mapDispatchToProps = dispatch => ({
     dispatch(contactActions.findContactsByName(event.target.value)),
 });
 
-// Filter.propTypes = {
-//   contactName: PropTypes.string.isRequired,
-//   onFindContact: PropTypes.func.isRequired,
-// };
+Filter.propTypes = {
+  contactName: PropTypes.string.isRequired,
+  onFindContact: PropTypes.func.isRequired,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Filter);
