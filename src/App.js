b30ba2f5e39d0761cf82fef7bfa3b0d7bb1dd419
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-01-08 23:17:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-// import { useEffect } from 'react';
 import './App.css';
 import Container from './components/Container/Container';
 import Section from './components/Section/Section';
-import ContactList from './components/ContactList/ContactList';
+import ContactList from './components/ContactList';
 import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from './components/Filter/Filter';
 
 export default function App() {
-  // useEffect(() => {
-  //   const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
-  //   parsedContacts && setContacts(parsedContacts);
-  // }, []);
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <main className="App">
       <Container>
@@ -32,8 +22,3 @@ export default function App() {
     </main>
   );
 }
-
-// App.defaultProps = {
-//   contacts: initialContacts,
-//   filter: '',
-// };
